bd0bf947623cdba7304ac7b54ec1106396c9da08
timilsinamohan <timilsinamohan@gmail.com>
2012-01-27 15:21:41 +0900

    added markov simulation in the main

--- all_in_one.cpp
+++ all_in_one.cpp
@@ -5,6 +5,11 @@
 #include <gtk/gtk.h>
 #include "co_ordinate_transformation.h"
 #include "check_gui.h"
+#include "final_gui.h"
+#include "degree_compute.h"
+#include "stochastic_matrix.h"
+#include "markov_simulation.h"
+
 
 
 
@@ -12,18 +17,29 @@ int main( int argc, char *argv[])
 
 {
  char *pFilename = argv[1];
- co_ordinate_transformation c1;
 
-c1.co_ordinate(pFilename);
-c1.drawing_points();
-check_gui ck;
-gtk_init(&argc, &argv);
-ck.calling_epidemic_gui();
+ co_ordinate_transformation c1;
+ c1.co_ordinate(pFilename);
+ c1.drawing_points();
+ degree_compute deg;
+ stochastic_matrix stochastic;
+ markov_simulation ms;
+ 
+ gtk_init(&argc, &argv);
+ final_gui fgui;
+ fgui.calling_epidemic_gui();
+
+
+fgui.file_read();
+
+
+deg.total_degree();
+stochastic.calculate_stochastic_matrix();
+ms.simulation_caller();
+
 gtk_main();
 return 0;
 }
-
-
 
 
 
